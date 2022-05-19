ef63234ae393cc9ca1bdec1925551699a7a3eec4
Nnamdi <nnamdi.b@gmail.com>
2022-05-19 21:41:24 +0200

    commented out some functions

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -25,25 +25,25 @@ export async function getServerSideProps({ params }) {
   };
 }
 
-export async function getStaticProps({ params }) {
-  const req = await fetch(`http://localhost:3000/${params.id}.json`);
-  const data = await req.json();
+// export async function getStaticProps({ params }) {
+//   const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//   const data = await req.json();
 
-  return {
-    props: { car: data },
-  };
-}
+//   return {
+//     props: { car: data },
+//   };
+// }
 
-export async function getStaticPaths() {
-  const req = await fetch('http://localhost:3000/cars.json');
-  const data = await req.json();
+// export async function getStaticPaths() {
+//   const req = await fetch('http://localhost:3000/cars.json');
+//   const data = await req.json();
 
-  const paths = data.map((car) => {
-    return { params: { id: car } };
-  });
+//   const paths = data.map((car) => {
+//     return { params: { id: car } };
+//   });
 
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
